b54551d246e77044346424230eff979996e9d227
dave.storkey <dave.storkey@metoffice.gov.uk>
2023-06-15 16:48:52 +0100

    bottom_field.py : update to correctly handle U-grid and V-grid fields.

--- src/bottom_field.py
+++ src/bottom_field.py
@@ -24,12 +24,15 @@ import netCDF4 as nc
 import numpy as np
 import numpy.ma as ma
 
-def bottom_field(infield=None,wgrid=False):
+def bottom_field(infield=None,tmask=None,grid=None):
+
+    if grid is None:
+        grid="T"
 
     if len(infield.shape) == 4:
         bottom_level = ma.zeros([infield.shape[i] for i in (0,2,3)])
         bottom_field = ma.zeros([infield.shape[i] for i in (0,2,3)])
-        if wgrid:
+        if grid == "W":
             # Special treatment required for W-grid fields, because field is not masked at the top of the bathymetry
             # and at least for W itself takes zero values here which aren't very interesting. We want to see the values
             # just above the bathymetry.
@@ -38,6 +41,26 @@ def bottom_field(infield=None,wgrid=False):
                                                   lev-1, bottom_level )
                 bottom_field[:,:,:] = np.where( (~infield[:,lev,:,:].mask & infield[:,lev+1,:,:].mask), 
                                                   infield[:,lev-1,:,:], bottom_field )
+        elif grid == "U":
+            # For U-grid fields need to use the T-mask to determine bottom points because U fields are set 
+            # to zero rather than masked at normal edge points.
+            for lev in range(len(infield[0,:,0,0])-1):
+                bottom_level[:,:,:] = np.where( ( ( tmask[0,lev  ,:,:] & np.roll( tmask[0,lev  ,:,:],-1,1)) & \
+                                                  (~tmask[0,lev+1,:,:] | np.roll(~tmask[0,lev+1,:,:],-1,1))), 
+                                                  lev, bottom_level )
+                bottom_field[:,:,:] = np.where( ( ( tmask[0,lev  ,:,:] & np.roll( tmask[0,lev  ,:,:],-1,1)) & \
+                                                  (~tmask[0,lev+1,:,:] | np.roll(~tmask[0,lev+1,:,:],-1,1))), 
+                                                  infield[:,lev,:,:], bottom_field )
+        elif grid == "V":
+            # For V-grid fields need to use the T-mask to determine bottom points because V fields are set
+            # to zero rather than masked at normal edge points. 
+            for lev in range(len(infield[0,:,0,0])-1):
+                bottom_level[:,:,:] = np.where( ( ( tmask[0,lev  ,:,:] & np.roll( tmask[0,lev  ,:,:],-1,0)) & \
+                                                  (~tmask[0,lev+1,:,:] | np.roll(~tmask[0,lev+1,:,:],-1,0))), 
+                                                  lev, bottom_level )
+                bottom_field[:,:,:] = np.where( ( ( tmask[0,lev  ,:,:] & np.roll( tmask[0,lev  ,:,:],-1,0)) & \
+                                                  (~tmask[0,lev+1,:,:] | np.roll(~tmask[0,lev+1,:,:],-1,0))), 
+                                                  infield[:,lev,:,:], bottom_field )
         else:
             for lev in range(len(infield[0,:,0,0])-1):
                 bottom_level[:,:,:] = np.where( (~infield[:,lev,:,:].mask & infield[:,lev+1,:,:].mask), 
@@ -48,16 +71,34 @@ def bottom_field(infield=None,wgrid=False):
     elif len(infield.shape) == 3:
         bottom_level = ma.zeros([infield.shape[i] for i in (1,2)])
         bottom_field = ma.zeros([infield.shape[i] for i in (1,2)])
-        if wgrid:
+        if grid == "W":
             # Special treatment required for W-grid fields, because field is not masked at the top of the bathymetry
             # and at least for W itself takes zero values here which aren't very interesting. We want to see the values
             # just above the bathymetry.
             for lev in [z+1 for z in range(len(infield[:,0,0])-2)]:
-                bottom_level[:,:] = np.where( (~infield[lev,:,:].mask & infield[lev+1,:,:].mask), 
+                bottom_level[:,:] = np.where( (~infield[lev,:,:].mask & infield[lev+1,:,:].mask),
                                                   lev-1, bottom_level )
                 bottom_field[:,:] = np.where( (~infield[lev,:,:].mask & infield[lev+1,:,:].mask), 
                                                  infield[lev-1,:,:], bottom_field )
-        else:
+        elif grid == "U":
+            # For U-grid fields need to use the T-mask to determine bottom points
+            for lev in range(len(infield[:,0,0])-1):
+                bottom_level[:,:] = np.where( ( ( tmask[0,lev  ,:,:] & np.roll( tmask[0,lev  ,:,:],-1,1)) & \
+                                                (~tmask[0,lev+1,:,:] | np.roll(~tmask[0,lev+1,:,:],-1,1))), 
+                                                  lev, bottom_level )
+                bottom_field[:,:] = np.where( ( ( tmask[0,lev  ,:,:] & np.roll( tmask[0,lev  ,:,:],-1,1)) & \
+                                                (~tmask[0,lev+1,:,:] | np.roll(~tmask[0,lev+1,:,:],-1,1))), 
+                                                 infield[lev,:,:], bottom_field )
+        elif grid == "V":
+            # For V-grid fields need to use the T-mask to determine bottom points
+            for lev in range(len(infield[:,0,0])-1):
+                bottom_level[:,:] = np.where( ( ( tmask[0,lev  ,:,:] & np.roll( tmask[0,lev  ,:,:],-1,0)) & \
+                                                (~tmask[0,lev+1,:,:] | np.roll(~tmask[0,lev+1,:,:],-1,0))), 
+                                                  lev, bottom_level )
+                bottom_field[:,:] = np.where( ( ( tmask[0,lev  ,:,:] & np.roll( tmask[0,lev  ,:,:],-1,0)) & \
+                                                (~tmask[0,lev+1,:,:] | np.roll(~tmask[0,lev+1,:,:],-1,0))), 
+                                                 infield[lev,:,:], bottom_field )
+        else: # T-grid default
             for lev in range(len(infield[:,0,0])-1):
                 bottom_level[:,:] = np.where( (~infield[lev,:,:].mask & infield[lev+1,:,:].mask), 
                                                   lev, bottom_level )
@@ -69,7 +110,21 @@ def bottom_field(infield=None,wgrid=False):
     return bottom_level,bottom_field
 
 
-def bottom_field_wrapper(filename,fieldname,wgrid=False,level=False,out3D=False):
+def bottom_field_wrapper(filename,fieldname,maskfile=None,grid=None,level=False,out3D=False):
+
+    if grid is None:
+        grid="T"
+
+    if maskfile is not None:
+        with nc.Dataset(maskfile,"r") as inmask:
+            try:
+                tmask = inmask.variables["tmask"][:]
+            except KeyError:
+                raise Exception("Error: Could not find tmask field in "+maskfile)
+    elif grid == "U" or grid == "V":
+        raise Exception("Error: mask file (containing tmask field) required for U- or V-grid fields.")
+    else:
+        tmask=None
 
     infile = nc.Dataset(filename,'r')
     field = infile.variables[fieldname]
@@ -124,7 +179,7 @@ def bottom_field_wrapper(filename,fieldname,wgrid=False,level=False,out3D=False)
     else:
         bot_level = ma.zeros(bot_field2D[:].shape)
 
-    bot_level[:], bot_field2D[:] = bottom_field(infield=infile.variables[fieldname][:],wgrid=wgrid)
+    bot_level[:], bot_field2D[:] = bottom_field(infield=infile.variables[fieldname][:],grid=grid,tmask=tmask)
 
     if out3D:
         if len(field.dimensions) == 4:
@@ -156,19 +211,58 @@ def bottom_field_wrapper(filename,fieldname,wgrid=False,level=False,out3D=False)
 
     # Copy in all the coordinate and bounds variables and their attributes
 
+    varlist=[]
+    for var in 'time_centered','time','time_counter','time_instant':
+        try:
+            invar=infile.variables[var]
+        except KeyError:
+            pass
+        else:
+            break
+    else:
+        raise Exception('Could not find time coordinate in input file.')
+    varlist+=[var]
+    for var in 'nav_lon','nav_lon_grid_T','nav_lon_grid_U','nav_lon_grid_V':
+        try:
+            invar=infile.variables[var]
+        except KeyError:
+            pass
+        else:
+            break
+    else:
+        raise Exception('Could not find longitude coordinate in input file.')
+    varlist+=[var]
+    for var in 'nav_lat','nav_lat_grid_T','nav_lat_grid_U','nav_lat_grid_V':
+        try:
+            invar=infile.variables[var]
+        except KeyError:
+            pass
+        else:
+            break
+    else:
+        raise Exception('Could not find latitude coordinate in input file.')
+    varlist+=[var]
     if out3D:
-        for depvar in 'depth','deptht','depthu','depthv':
+        for var in 'depth','deptht','depthu','depthv':
             try:
-                invar=infile.variables[depvar]
+                invar=infile.variables[var]
             except KeyError:
                 pass
             else:
                 break
         else:
             raise Exception('Could not find depth coordinate in input file.')
-        varlist = ['nav_lat','nav_lon',depvar]
-    else:
-        varlist = ['nav_lat','nav_lon']
+        varlist+=[var]
+        for var in 'e3t','e3u','e3v','thkcello':
+            try:
+                invar=infile.variables[var]
+            except KeyError:
+                pass
+            else:
+                break
+        else:
+            raise Exception('Could not find thickness variable in input file.')
+        varlist+=[var]
 
     for var in varlist:
         invar=infile.variables[var]
@@ -179,9 +273,6 @@ def bottom_field_wrapper(filename,fieldname,wgrid=False,level=False,out3D=False)
             attrdict[attr.encode('ascii')] = invar.getncattr(attr)
         outfile.variables[var].setncatts(attrdict)
 
-         
-
-
     infile.close()
     outfile.close()
 
@@ -190,8 +281,10 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="name of input file")
     parser.add_argument("fieldname", help="name of field to extract")
-    parser.add_argument("-W", "--wgrid", action="store_true",dest="wgrid",
-                    help="W-grid field")
+    parser.add_argument("-M", "--maskfile", action="store",dest="maskfile",
+                    help="Name of file containing tmask field (usually mesh mask file).")
+    parser.add_argument("-G", "--grid", action="store",dest="grid",
+                    help="Which grid is field on? T (default), U, V, W. If U or V, meshmask file required.")
     parser.add_argument("-L", "--level", action="store_true",dest="level",
                     help="Output index of bottom level as well as bottom field value")
     parser.add_argument("--3D", action="store_true",dest="out3D",
@@ -199,4 +292,4 @@ if __name__=="__main__":
 
     args = parser.parse_args()
 
-    bottom_field_wrapper(args.filename,args.fieldname,wgrid=args.wgrid,level=args.level,out3D=args.out3D)
+    bottom_field_wrapper(args.filename,args.fieldname,grid=args.grid,maskfile=args.maskfile,level=args.level,out3D=args.out3D)
