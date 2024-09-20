4e2833a7027bb749c4180536497bdd5bb2fc7fd7
DaveStorkey <dave.storkey@metoffice.gov.uk>
2024-09-20 16:04:37 +0100

    vert_integrate.py : 1. Add option to mask using mask from meshmask file. 2. Add error check for levels option which isn't working.

--- src/vert_integrate.py
+++ src/vert_integrate.py
@@ -11,7 +11,10 @@ import xarray as xr
 import numpy as np
 
 def vert_integrate(file_in=None, var_in=None, depvar=None, file_out=None, var_out=None, levels=None, 
-                   factor=None, avg=None):
+                   factor=None, avg=None, meshfile=None, meshvar=None):
+
+    if levels is not None:
+        raise Exception("Error: option to select a range of levels not working yet!")
 
     if not file_in or not var_in or not file_out:
         raise Exception("Error: must specify file_in, var_in and file_out.")
@@ -37,6 +40,14 @@ def vert_integrate(file_in=None, var_in=None, depvar=None, file_out=None, var_ou
     if not factor:
         factor=1.0
 
+    mshfld=None
+    if meshfile is not None and meshvar is not None:
+        with xr.open_dataset(meshfile) as inmesh:
+            try:
+                mshfld = getattr(inmesh,meshvar)
+            except AttributeError:
+                raise Exception("Can't find "+meshvar+" in mesh file "+meshfile)
+
     for invar,outvar in zip(var_in,var_out):
         with xr.open_dataset(file_in) as indata:
             try:
@@ -55,16 +66,20 @@ def vert_integrate(file_in=None, var_in=None, depvar=None, file_out=None, var_ou
 
         # vertically integrate
         field_int3D = field * thick * factor
+        if mshfld is not None:
+            field_int3D = field_int3D * mshfld
         depths = field.coords[depvar]
         if avg:
             if levels:
-                thick_int = xr.where(depths==slice(levels[0],levels[1]),thick,0.0).sum(dim=depvar)
+#                thick_int = xr.where(depths==slice(levels[0],levels[1]),thick,0.0).sum(dim=depvar)
+                thick_int = thick.isel(depvar=slice(levels[0],levels[1])).sum(dim=depvar)
             else:
                 thick_int = thick.sum(dim=depvar)
         else:
             thick_int = 1.0
         if levels:
-            field_int = xr.where(depths==slice(levels[0],levels[1]),field_int3D,0.0).sum(dim=depvar) / thick_int
+#            field_int = xr.where(depths==slice(levels[0],levels[1]),field_int3D,0.0).sum(dim=depvar) / thick_int
+            field_int = field_int3D.isel(depvar=slice(levels[0],levels[1])).sum(dim=depvar)
         else:
             field_int = field_int3D.sum(dim=depvar) / thick_int
 
@@ -72,6 +87,9 @@ def vert_integrate(file_in=None, var_in=None, depvar=None, file_out=None, var_ou
             outdata = field_int.to_dataset(name=outvar)
         else:
             outdata[outvar] = field_int
+
+        # apply surface mask from input field
+        outdata[outvar] = xr.where(field[0].isnull(), np.nan, outdata[outvar])
 
     outdata.to_netcdf(file_out)
                 
@@ -94,10 +112,14 @@ if __name__=="__main__":
                          help="scalar multiplicative factor")
     parser.add_argument("-A", "--avg", action="store_true",dest="avg",
                          help="calculate depth mean rather than depth integral")
+    parser.add_argument("-M", "--meshfile", action="store",dest="meshfile",
+                         help="name of mesh-mask file")
+    parser.add_argument("-m", "--meshvar", action="store",dest="meshvar",
+                         help="name of variable from mesh-mask file to use for weighting")
 
     args = parser.parse_args()
 
     vert_integrate(file_in=args.file_in,var_in=args.var_in,depvar=args.depvar,
-                   file_out=args.file_out,var_out=args.var_out,
+                   file_out=args.file_out,var_out=args.var_out,meshfile=args.meshfile,meshvar=args.meshvar,
                    levels=args.levels,factor=args.factor,avg=args.avg)
 
